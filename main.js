e95ba68ac03ac94e5871a65fc18f2166a86a49a9
Andrew Napoleone <Naps277@gmail.com>
2023-11-05 11:28:42 -0500

    Added submit functions

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
-/* ATTENTION: THIS IS CODE FROM THE YOUTUBE CRASH COURSE. IT IS NOT MEANT TO RUN, IT IS JUST FOR LEARNING PURPOSES 
+/* ATTENTION: THIS IS CODE FROM THE YOUTUBE CRASH COURSE. IT IS NOT MEANT TO RUN, IT IS JUST FOR LEARNING PURPOSES
 
 Video by Brad Traversy
 JavaScript Crash Course For Beginners
-https://youtu.be/hdI2bqOjy3c?t=4228 
+https://youtu.be/hdI2bqOjy3c?t=4228
 
 Start at 1:10:29
  */
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  if (nameInput.value === '' || emailInput.value === '') {
+    msg.classList.add('error');
+    msg.innerHTML = 'Please enter all fields';
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+    userList.appendChild(li);
+
+    nameInput.value = '';
+    emailInput.value = '';
+  }
+}
